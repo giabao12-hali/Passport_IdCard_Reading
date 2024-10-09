5a41a8a550554f433870880f4c50192be132a075
Gia Bao <nguyenhiengiabao12@gmail.com>
2024-10-09 14:40:32 +0700

    loading button

--- src/components/passport.jsx
+++ src/components/passport.jsx
@@ -522,19 +522,29 @@ const PassportRead = () => {
                             {loadingPassports ? (
                                 <>
                                     <div>
-                                        <button className="btn btn-accent btn-disabled rounded-xl no-animation mobile:h-auto mobile:text-balance" onClick={handleSave}>Lưu</button>
+                                        <button className="btn btn-accent btn-disabled rounded-xl no-animation mobile:h-auto mobile:text-balance" onClick={handleSave}>
+                                            <span className="loading loading-spinner"></span>
+                                            Lưu
+                                        </button>
                                     </div>
                                     <div>
-                                        <button className="btn btn-accent btn-disabled rounded-xl no-animation mobile:h-auto mobile:text-balance" onClick={handleCopyToClipboard}>Lưu và cập nhật eTour</button>
+                                        <button className="btn btn-accent btn-disabled rounded-xl no-animation mobile:h-auto mobile:text-balance" onClick={handleCopyToClipboard}>
+                                            <span className="loading loading-spinner"></span>
+                                            Lưu và cập nhật eTour
+                                        </button>
                                     </div>
                                 </>
                             ) : (
                                 <>
                                     <div>
-                                        <button className="btn btn-success no-animation rounded-xl mobile:h-auto mobile:text-balance" onClick={handleSave}>Lưu</button>
+                                        <button className="btn btn-success no-animation rounded-xl mobile:h-auto mobile:text-balance" onClick={handleSave}>
+                                            Lưu
+                                        </button>
                                     </div>
                                     <div>
-                                        <button className="btn btn-accent no-animation rounded-xl mobile:h-auto mobile:text-balance" onClick={handleCopyToClipboard}>Lưu và cập nhật eTour</button>
+                                        <button className="btn btn-accent no-animation rounded-xl mobile:h-auto mobile:text-balance" onClick={handleCopyToClipboard}>
+                                            Lưu và cập nhật eTour
+                                        </button>
                                     </div>
                                     <div>
                                         <button className="btn btn-info no-animation rounded-xl mobile:h-auto mobile:text-balance" onClick={() => handleImageClick(previewImage[0])}>Xem hình ảnh</button>
